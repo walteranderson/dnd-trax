840f7b72e11dcdcaa3890b0a0bd84b6cce5f536f
Walter Anderson <walteranderson19@gmail.com>
2018-10-14 22:41:36 -0500

    adding comments with potential character properties

--- src/characters/character.model.js
+++ src/characters/character.model.js
@@ -1,5 +1,24 @@
 import { Model } from 'objection'
 
+// level
+// xp
+// proficiency bonus?
+// class
+// race
+// subrace
+// name
+// age
+// languages?
+// base speed
+// ability scores
+// background
+// personality traits (2)
+// chosen proficiencies
+// starting equipment
+// alignment
+// max hitpoints?
+// money
+
 export default class Character extends Model {
   static tableName = 'characters'
 
